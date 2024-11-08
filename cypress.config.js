6623ad699a6bf47a42edefc120c875d7d88191e4
viviannygard <vnaygard@gmail.com>
2024-11-08 16:43:04 -0300

    Delete cypress.config.js

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-const { defineConfig } = require('cypress')
-
-module.exports = defineConfig({
-  viewportHeight: 880,
-  viewportWidth: 1280,
-  e2e: {
-    setupNodeEvents(on, config) {},
-  },
-})
